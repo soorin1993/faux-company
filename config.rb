f27a5a6a69f891b2c091711d905aced6d908da69
Soo Park <soorin1993@gmail.com>
2016-10-27 09:15:42 -0600

    done

--- config.rb
+++ config.rb
@@ -1,21 +1,27 @@
 require 'compass/import-once/activate'
+module Compass::ImportOnce::Importer
+  def find(uri, options, *args)
+    uri, force_import = handle_force_import(uri.gsub(/^\(NOT IMPORTED\) /, ''))
+    maybe_replace_with_dummy_engine(super(uri, options, *args), options, force_import)
+  end
+end
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
---
+++ js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $("#vid-container").fitVids();
+});
